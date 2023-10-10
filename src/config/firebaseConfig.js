d51f897b1629f9f65ae8075acf231266c015ef8e
Muath A. Zoubi <32450805+muathzoubi@users.noreply.github.com>
2023-10-10 22:59:19 +0300

    Initial commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBP9StwYM8qoOmufb9hQXVKClsX7NFxZqA",
+  authDomain: "cahtly-app.firebaseapp.com",
+  databaseURL: "https://cahtly-app-default-rtdb.firebaseio.com",
+  projectId: "cahtly-app",
+  storageBucket: "cahtly-app.appspot.com",
+  messagingSenderId: "593203560564",
+  appId: "1:593203560564:web:545e6af5a8bfb77daf1121",
+  measurementId: "G-VGH99B2Y1Y"
+};
+
+export default firebaseConfig;
